d44aa4243b078480777a18d507a72f8d65834f5e
Alexander Evans <sander7989@gmail.com>
2025-08-26 16:36:01 -0600

    seed random number generator

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /*
  This program generates a requested number of names using a pattern of vowels, consonants, prefixes, and suffixes to simulate a fantasy language naming scheme.
  */
+
+char vowels[][2] = {{"a"}, {"e"}, {"i"}, {"o"}, {"u"}, {"y"}, {"aa"}, {"ee"}, {"ii"},
+                    {"oo"}, {"uu"}, {"yy"}, {"ae"}, {"ui"}, {"ie"}, {"ei"}, {"oi"}, {"io"},
+                    {"eo"}, {"oe"}, {"ey"}, {"ye"}};
+
+char consonants[][2] = {{"q"}, {"w"}, {"r"}, {"t"}, {"y"}, {"p"}, {"s"}, {"d"}, {"f"},
+                        {"g"}, {"h"}, {"j"}, {"k"}, {"l"}, {"z"}, {"x"}, {"c"}, {"v"},
+                        {"b"}, {"n"}, {"m"}, {"qu"}, {"ll"}};
+
+char prefixes[6][6] = {{""}};
+char suffixes[6][6] = {{""}};
 
 int main(int argc, char* argv[]){
 
@@ -11,14 +24,25 @@ int main(int argc, char* argv[]){
 
     //Get the number of desired names to be generated
     if (argc > 1) {
-        nameCount = strtoimax(argv[1], NULL, 10);
+        nameCount = (int)strtoimax(argv[1], NULL, 10);
 
         //Bounds checking
         nameCount < 0 ? nameCount = 0 : 0;
         nameCount > 128 ? nameCount = 128 : 0;
     }
 
-    printf("%d", nameCount);
+    printf("%d\n", nameCount);
+
+    long seed = time(NULL);
+
+    //Get the random seed if provided
+    if (argc > 2) {
+        seed = strtoimax(argv[2], NULL, 10);
+    }
+
+    srand(seed);
+
+    //printf("%ld\n", seed);
 
     return 0;
 }
